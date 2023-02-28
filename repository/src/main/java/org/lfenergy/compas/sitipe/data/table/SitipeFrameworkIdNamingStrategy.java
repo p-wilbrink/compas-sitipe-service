3c39e1153c7dd8431362829f4b0f19dce0d209a5
Pascal Wilbrink <pascal.wilbrink@alliander.com>
2023-02-28 11:29:20 +0100

    Added SPDX headers
    Signed-off-by: Pascal Wilbrink <pascal.wilbrink@alliander.com>

--- repository/src/main/java/org/lfenergy/compas/sitipe/data/table/SitipeFrameworkIdNamingStrategy.java
+++ repository/src/main/java/org/lfenergy/compas/sitipe/data/table/SitipeFrameworkIdNamingStrategy.java
@@ -1,11 +1,13 @@
+// SPDX-FileCopyrightText: 2023 Alliander N.V.
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package org.lfenergy.compas.sitipe.data.table;
 
 import org.eclipse.microprofile.config.ConfigProvider;
 import org.hibernate.boot.model.naming.Identifier;
 import org.hibernate.boot.model.naming.PhysicalNamingStrategyStandardImpl;
 import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
-
-
 
 public class SitipeFrameworkIdNamingStrategy extends PhysicalNamingStrategyStandardImpl {
 
